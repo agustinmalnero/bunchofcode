f16b387b874c73c4f8070ded52e0f147020ae29f
Diego Sarmentero <diego.sarmentero@gmail.com>
2010-09-11 21:48:36 -0300

    end

--- TSB-tp1/src/tsbtp1/Main.java
+++ TSB-tp1/src/tsbtp1/Main.java
@@ -1,6 +1,4 @@
 package tsbtp1;
-import tsbtp1.model.Pasajero;
-import tsbtp1.util.*;
 
 /**
  *
@@ -9,37 +7,8 @@ import tsbtp1.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        SimpleList<Pasajero> l = new SimpleList<Pasajero>();
-        l.add(new Pasajero("31756403", "Sarmentero", "Diego"));
-        l.add(new Pasajero("33544123", "Otro", "Mas"));
-        l.add(new Pasajero("11111222", "addfdsf", "dsfdsd"));
-        l.add(new Pasajero("11222111", "Apellido", "Nombre"));
-        Iterator<Pasajero> it = l.iterator();
-        while (it.hasNext())
-            System.out.println(it.next());
-<<<<<<< local
-        while (it.hasNext())
-            System.out.println(it.next());
-        if (l.remove(3))
-            System.out.println("removido 3");
-        else System.out.println("no removido 3");
-        if (l.remove(3))
-            System.out.println("removido 3");
-        else System.out.println("no removido 3");
-        while (it.hasNext())
-            System.out.println(it.next());
         AgenciaFrame agencia = new AgenciaFrame();
         agencia.setVisible(true);
-=======
-        /*AgenciaFrame agencia = new AgenciaFrame();
-        agencia.setVisible(true);*/
->>>>>>> other
-        System.out.println("Ordenar...");
-        Comparable[] array = l.ordenar();
-        System.out.println("Finalizó Ordenar.");
-        for(Comparable c : array){
-            System.out.println(c);
-        }
     }
 
 }
